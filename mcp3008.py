fa823ad3d699beaa0490a9fb95529afd3c06447e
jy950902 <30822575+jy950902@users.noreply.github.com>
2020-02-25 14:56:53 +0900

    Update mcp3008.py

--- mcp3008.py
+++ mcp3008.py
@@ -2,6 +2,7 @@ from spidev import SpiDev
 import RPi.GPIO as GPIO
 import time
 
+# ADC 컨버터 
 class MCP3008:
     def __init__(self, bus = 0, device = 0):
         self.bus, self.device = bus, device
@@ -18,6 +19,7 @@ class MCP3008:
 
     def close(self):
         self.spi.close()
+
 LED=16   
 pir_s=25
 GPIO.setmode(GPIO.BCM)
@@ -28,19 +30,19 @@ adc = MCP3008()
 
 try:
     while True:
-        adc_value = adc.read( channel = 0 )
-        if GPIO.input(pir_s) == True:
+        adc_value = adc.read( channel = 0 ) # 조도센서 값 읽어오기
+        if GPIO.input(pir_s) == True: # 사람이 pir 센서에 감지되면
             print("Pir On")
-            if (adc_value == 0):
+            if (adc_value == 0): # 조도센서 값이 0이면 (=>어두우면)
                 print("LED ON : PIR %d"%adc_value)
-                GPIO.output(LED, True)
+                GPIO.output(LED, True) # 불이 켜짐
                 time.sleep(2)
             else:
-                print("LED OFF : PIR %d"%adc_value)
-                GPIO.output(LED, False)
+                print("LED OFF : PIR %d"%adc_value) # 조도센서 값이 0이 아니면 (=>빛이 조금이라도 있으면)
+                GPIO.output(LED, False) # 불이 꺼짐
                 time.sleep(2)
         else:
-            print("Pir Off")
+            print("Pir Off") # 사람이 감지되지 않음
             GPIO.output(LED, False)
 
 
